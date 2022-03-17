fad964a19709de3366000ed163b87baac8cfe9ae
Kaique <kaiquevan@hotmail.com>
2022-03-17 17:47:47 -0300

    Implementando uma calculadora

---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/CaculatorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CaculatorController : ControllerBase
+    {
+
+
+        private readonly ILogger<CaculatorController> _logger;
+
+        public CaculatorController(ILogger<CaculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if( decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        } 
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo,  out number);
+            return isNumber;
+        }
+    }
+}
