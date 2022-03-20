d0944dcde277853211bcb205a44efdfd25c83f42
Kaique <kaiquevan@hotmail.com>
2022-03-20 18:59:35 -0300

    Subida teste pelo visual

--- RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/PersonController.cs
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/PersonController.cs
@@ -9,13 +9,13 @@ namespace RestWithASP_NET5Udemy.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CaculatorController : ControllerBase
+    public class PersonController : ControllerBase
     {
 
 
-        private readonly ILogger<CaculatorController> _logger;
+        private readonly ILogger<PersonController> _logger;
 
-        public CaculatorController(ILogger<CaculatorController> logger)
+        public PersonController(ILogger<PersonController> logger)
         {
             _logger = logger;
         }
@@ -31,21 +31,6 @@ namespace RestWithASP_NET5Udemy.Controllers
             return BadRequest("Invalid input");
         }
 
-        private decimal ConvertToDecimal(string strNumber)
-        {
-            decimal decimalValue;
-            if( decimal.TryParse(strNumber, out decimalValue))
-            {
-                return decimalValue;
-            }
-            return 0;
-        } 
 
-        private bool IsNumeric(string strNumber)
-        {
-            double number;
-            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo,  out number);
-            return isNumber;
-        }
     }
 }
---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Adders { get; set; }
+
+        public string Gender { get; set; }
+
+
+    }
+}
